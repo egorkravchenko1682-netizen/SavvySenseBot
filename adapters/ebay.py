18c12c15cb1d62522a1dea9cdd9f2460c7ba7e14
egorkravchenko1682-netizen <egorkravchenko1682@gmail.com>
2026-09-10 11:41:33 +0300

    Create ebay.py

---
+++ adapters/ebay.py
@@ -0,0 +1,26 @@
+from products import Product
+from adapters.base import ShopAdapter
+
+
+class EbayAdapter(ShopAdapter):
+
+    @property
+    def shop_name(self) -> str:
+        return "ebay"
+
+    def can_handle(self, url: str) -> bool:
+        domains = [
+            "ebay.com",
+            "ebay.de",
+            "ebay.fr",
+        ]
+
+        url = url.lower()
+
+        return any(domain in url for domain in domains)
+
+    def get_product(self, url: str) -> Product | None:
+        return None
+
+    def search(self, query: str) -> list[Product]:
+        return []
