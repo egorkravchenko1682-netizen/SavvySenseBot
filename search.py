91383aec3e609dcb52ff618fd3a837078a89acac
egorkravchenko1682-netizen <egorkravchenko1682@gmail.com>
2026-09-10 11:39:09 +0300

    Create search.py

---
+++ search.py
@@ -0,0 +1,52 @@
+from typing import List
+
+from products import Product
+from adapters.base import ShopAdapter
+from adapters.wildberries import WildberriesAdapter
+
+
+class GlobalSearch:
+
+    def __init__(self):
+        self.adapters: List[ShopAdapter] = [
+            WildberriesAdapter(),
+        ]
+
+    def get_product_from_link(self, url: str) -> Product | None:
+        for adapter in self.adapters:
+            if adapter.can_handle(url):
+                return adapter.get_product(url)
+
+        return None
+
+    def search_everywhere(self, query: str) -> List[Product]:
+        results = []
+
+        for adapter in self.adapters:
+            try:
+                products = adapter.search(query)
+
+                if products:
+                    results.extend(products)
+
+            except Exception as e:
+                print(
+                    f"{adapter.shop_name} search error:",
+                    e
+                )
+
+        return results
+
+    def sort_by_price(
+        self,
+        products: List[Product]
+    ) -> List[Product]:
+
+        return sorted(
+            products,
+            key=lambda product: (
+                product.price
+                if product.price is not None
+                else float("inf")
+            )
+        )
