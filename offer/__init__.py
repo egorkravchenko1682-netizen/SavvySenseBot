d3984f0c107001de118932b48e809497140f42c4
egorkravchenko1682-netizen <egorkravchenko1682@gmail.com>
2026-09-22 22:27:48 +0300

    Update __init__.py

--- offer/__init__.py
+++ offer/__init__.py
@@ -1,6 +1,15 @@
-from .normalizer import normalize_offer, normalize_offers
+from .normalizer import (
+    normalize_offer,
+    normalize_offers,
+)
+
+from .extractor import (
+    OfferExtractor,
+)
+
 
 __all__ = [
     "normalize_offer",
     "normalize_offers",
+    "OfferExtractor",
 ]
