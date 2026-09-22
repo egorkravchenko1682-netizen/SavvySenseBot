ff21b01eaf7b441666c825089b251a772c25134b
egorkravchenko1682-netizen <egorkravchenko1682@gmail.com>
2026-09-22 22:45:47 +0300

    Update __init__.py

--- product/__init__.py
+++ product/__init__.py
@@ -1,15 +1,12 @@
 from .identity import identify_product
 from .dna import build_product_dna
-
-from .query import (
-    build_search_queries,
-    build_search_plan,
-)
+from .query import build_search_plan
+from .attributes import AttributeExtractor
 
 
 __all__ = [
     "identify_product",
     "build_product_dna",
-    "build_search_queries",
     "build_search_plan",
+    "AttributeExtractor",
 ]
