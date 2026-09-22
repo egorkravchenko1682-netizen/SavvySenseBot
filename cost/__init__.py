f12b6744344e1f22999c9b7e474a96ee4e8a76dd
egorkravchenko1682-netizen <egorkravchenko1682@gmail.com>
2026-09-22 22:15:50 +0300

    Update __init__.py

--- cost/__init__.py
+++ cost/__init__.py
@@ -1,5 +1,9 @@
-from .calculator import calculate_real_cost
+from .calculator import (
+    calculate_real_cost,
+    calculate_offers_real_cost,
+)
 
 __all__ = [
     "calculate_real_cost",
+    "calculate_offers_real_cost",
 ]
