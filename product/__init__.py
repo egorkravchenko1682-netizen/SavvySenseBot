77ecb1b36e01b14657d06991529a348f0f4a2053
egorkravchenko1682-netizen <egorkravchenko1682@gmail.com>
2026-09-22 21:05:16 +0300

    Update __init__.py

--- product/__init__.py
+++ product/__init__.py
@@ -1,7 +1,13 @@
 from .identity import identify_product
 from .dna import build_product_dna
+from .query import (
+    build_search_queries,
+    build_search_plan,
+)
 
 __all__ = [
     "identify_product",
     "build_product_dna",
+    "build_search_queries",
+    "build_search_plan",
 ]
