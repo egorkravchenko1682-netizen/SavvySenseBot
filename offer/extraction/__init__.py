7a82f529e05e16d6b8f189d39667a3f7f41cccf7
egorkravchenko1682-netizen <egorkravchenko1682@gmail.com>
2026-09-23 17:16:09 +0300

    Update __init__.py

--- offer/extraction/__init__.py
+++ offer/extraction/__init__.py
@@ -4,3 +4,22 @@ SAVVY SENSE Offer Extraction.
 Модули этого пакета отвечают за извлечение
 структурированных данных из найденных предложений.
 """
+
+from .base import BaseExtractor
+from .product import ProductExtractor
+from .attributes import AttributeExtractor
+from .price import PriceExtractor
+from .condition import ConditionExtractor
+from .availability import AvailabilityExtractor
+from .structured_data import StructuredDataExtractor
+
+
+__all__ = [
+    "BaseExtractor",
+    "ProductExtractor",
+    "AttributeExtractor",
+    "PriceExtractor",
+    "ConditionExtractor",
+    "AvailabilityExtractor",
+    "StructuredDataExtractor",
+]
